87db7df9c80aaf9016118154fc6a414738f5fb0b
Alberto González <10727467+agonper@users.noreply.github.com>
2022-04-29 19:26:27 +0100

    chore: update demo with geolocation tasks

--- apps/demo/src/plugin-demos/geolocation.ts
+++ apps/demo/src/plugin-demos/geolocation.ts
@@ -1,6 +1,5 @@
-import { Observable, EventData, Page } from '@nativescript/core';
+import { EventData, Page } from '@nativescript/core';
 import { DemoSharedGeolocation } from '@demo/shared';
-import {} from '@awarns/geolocation';
 
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object;
