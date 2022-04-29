ae7df4975b2bbf6bb8a502e50a75037f08302902
Alberto González <10727467+agonper@users.noreply.github.com>
2022-04-29 10:20:34 +0100

    refactor(geofen): extract AoIs from entities API

--- packages/geofencing/entities/index.ts
+++ packages/geofencing/entities/index.ts
@@ -1,6 +1,2 @@
 export * from '../internal/entities/aoi';
 export { AoIProximityChange, GeofencingProximity } from '../internal/entities';
-
-import { AreasOfInterestStore as AOIS, areasOfInterestStoreDB as areasOfInterest } from '../internal/persistence';
-export type AreasOfInterest = AOIS;
-export { areasOfInterest };
--- packages/geofencing/index.ts
+++ packages/geofencing/index.ts
@@ -1,2 +1,3 @@
 export * from './entities';
 export * from './tasks';
+export * from './store';
