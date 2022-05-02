2fde0cfe1535222f57fff01ad16875d572258dd9
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-02 19:32:22 +0100

    chore: get tracing back working in demo

--- tools/demo/tasks.ts
+++ tools/demo/tasks.ts
@@ -1,8 +1,20 @@
 import { Task } from 'nativescript-task-dispatcher/tasks';
-import { startDetectingCoarseHumanActivityChangesTask, stopDetectingCoarseHumanActivityChangesTask } from '@awarns/human-activity';
+import {
+  startDetectingCoarseHumanActivityChangesTask,
+  stopDetectingCoarseHumanActivityChangesTask,
+} from '@awarns/human-activity';
 import { acquireMultiplePhoneGeolocationTask, acquirePhoneGeolocationTask } from '@awarns/geolocation';
 import { checkAreaOfInterestProximityTask } from '@awarns/geofencing';
 import { sendNotificationTask } from '@awarns/notifications';
-import { trackEventTask } from '@awarns/tracing';
+import { makeTraceable, trackEventTask } from '@awarns/tracing';
 
-export const demoTasks: Array<Task> = [startDetectingCoarseHumanActivityChangesTask(), stopDetectingCoarseHumanActivityChangesTask(), acquirePhoneGeolocationTask(/* optional */ { bestOf: 3, timeout: 10000 }), acquireMultiplePhoneGeolocationTask(/* optional */ { bestOf: 1, timeout: 15000 }), checkAreaOfInterestProximityTask(), sendNotificationTask(), trackEventTask()];
+export const demoTasks: Array<Task> = [
+  ...makeTraceable([startDetectingCoarseHumanActivityChangesTask(), stopDetectingCoarseHumanActivityChangesTask()]),
+  ...makeTraceable([
+    acquirePhoneGeolocationTask(/* optional */ { bestOf: 3, timeout: 10000 }),
+    acquireMultiplePhoneGeolocationTask(/* optional */ { bestOf: 1, timeout: 15000 }),
+    checkAreaOfInterestProximityTask(),
+    sendNotificationTask(),
+  ]),
+  trackEventTask(),
+];
