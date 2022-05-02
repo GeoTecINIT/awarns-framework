d682403132b80a8a1c6a56e6310afc7086755d9e
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-02 17:50:16 +0100

    refactor(geoloc): remove tracing presence

--- packages/geolocation/internal/tasks.ts
+++ packages/geolocation/internal/tasks.ts
@@ -10,14 +10,12 @@ const DEFAULT_BATCH_TIMEOUT = 15000;
 export function acquirePhoneGeolocationTask(config?: GeolocationTaskConfig): Task {
   return new SinglePullProviderTask(new GeolocationProvider(config.bestOf ?? DEFAULT_SINGLE_BEST_OF, config.timeout ?? DEFAULT_SINGLE_TIMEOUT), 'Phone', {
     foreground: true,
-    sensitiveData: true,
   });
 }
 
 export function acquireMultiplePhoneGeolocationTask(config?: GeolocationTaskConfig): Task {
   return new BatchPullProviderTask(new GeolocationProvider(config.bestOf ?? DEFAULT_BATCH_BEST_OF, config.timeout ?? DEFAULT_BATCH_TIMEOUT), 'Phone', {
     foreground: true,
-    sensitiveData: true,
   });
 }
 
