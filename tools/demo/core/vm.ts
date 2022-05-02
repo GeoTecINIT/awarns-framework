66f4d78b83e881e4a0a1205709a447e26e3e771f
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-02 19:59:13 +0100

    chore: revert extracting demo tracing code (unstable nested observables)

--- tools/demo/core/vm.ts
+++ tools/demo/core/vm.ts
@@ -1,30 +1,42 @@
 import { DemoSharedBase } from '../utils';
 import { NotificationCallbacks } from '../notifications/callbacks';
-import { TracesManager } from '../tracing/manager';
 
-import { alert, AlertOptions, confirm, ConfirmOptions } from '@nativescript/core';
+import { alert, AlertOptions, confirm, ConfirmOptions, knownFolders } from '@nativescript/core';
+
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime, map, switchMap } from 'rxjs/operators';
+
+import { createTracesExporter, ExportResult, Trace, TracesStore, tracesStore } from '@awarns/tracing';
 
 import { setupAreasOfInterest } from '../geofencing/setup';
 import { emitStartEvent } from './common';
 
 const SIZE_INCREMENT = 10;
 
+const EXPORT_FOLDER = knownFolders.documents().getFolder('Record logs');
+
 export class DemoSharedCore extends DemoSharedBase {
   notificationCallbacks = new NotificationCallbacks();
 
   get traces() {
-    return this.tracesManager.traces;
+    return this._traces;
   }
 
   get exportingTraces() {
-    return this.tracesManager.exportingTraces;
+    return this._exportingTraces;
   }
 
-  private tracesManager = new TracesManager();
+  private _traces = [];
   private _size = 0;
 
-  constructor() {
+  private _exportingTraces = false;
+
+  private _fetchOrders: Subject<number>;
+  private _subscription: Subscription;
+
+  constructor(private store: TracesStore = tracesStore) {
     super();
+    this.subscribeToDatabaseChanges();
     this.loadMore();
   }
 
@@ -40,8 +52,7 @@ export class DemoSharedCore extends DemoSharedBase {
   }
 
   handleExportTap() {
-    this.tracesManager
-      .exportTraces()
+    this.exportTraces()
       .then((result) => {
         const alertOptions: AlertOptions = {
           message: `Event log exported (${result.exportCount} exported)`,
@@ -63,7 +74,7 @@ export class DemoSharedCore extends DemoSharedBase {
     };
     confirm(confirmOptions).then((confirmed) => {
       if (confirmed) {
-        this.tracesManager.clearTraces();
+        this.clearTraces();
       }
     });
   }
@@ -71,6 +82,68 @@ export class DemoSharedCore extends DemoSharedBase {
   loadMore() {
     console.log(`Loading more items...`);
     this._size += SIZE_INCREMENT;
-    this.tracesManager.fetchMore(this._size);
+    this._fetchOrders.next(this._size);
   }
+
+  private exportTraces(): Promise<ExportResult> {
+    this.toggleExportingTraces(true);
+    return createTracesExporter(EXPORT_FOLDER)
+      .export()
+      .then((result) => {
+        this.toggleExportingTraces(false);
+        return result;
+      })
+      .catch((err) => {
+        this.toggleExportingTraces(false);
+        return err;
+      });
+  }
+
+  private clearTraces() {
+    console.warn('Up to clear traces!');
+    this.store.clear();
+  }
+
+  private subscribeToDatabaseChanges() {
+    this._fetchOrders = new Subject<number>();
+
+    const listTraces = (size: number) => this.store.list(size).pipe(map((traces) => traces.map(formatTrace)));
+
+    const stream = this._fetchOrders.pipe(debounceTime(1000), switchMap(listTraces));
+
+    this._subscription = stream.subscribe({
+      next: (traces) => {
+        this._traces = traces;
+        this.notifyPropertyChange('traces', traces);
+      },
+      error: (err) => console.error(`Error loading traces: ${err}`),
+    });
+  }
+
+  private toggleExportingTraces(value: boolean) {
+    this._exportingTraces = value;
+    this.notifyPropertyChange('exportingTraces', value);
+  }
+}
+
+function formatTrace(trace: Trace) {
+  return {
+    ...trace,
+    timestamp: timestampFormatter(trace.timestamp),
+  };
+}
+
+function timestampFormatter(t: Date) {
+  const day = twoDigits(t.getDate());
+  const month = twoDigits(t.getMonth() + 1);
+  const year = t.getFullYear();
+  const hour = twoDigits(t.getHours());
+  const minutes = twoDigits(t.getMinutes());
+  const seconds = twoDigits(t.getSeconds());
+
+  return `${day}/${month}/${year} ${hour}:${minutes}:${seconds}`;
+}
+
+function twoDigits(num: number): String {
+  return `0${num}`.slice(-2);
 }
