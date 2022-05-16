76c7fe27ab3ec99f8a8c15e50ca377541a6af79e
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-16 17:54:24 +0100

    feat(notif): add random sender task

--- packages/notifications/internal/tasks.ts
+++ packages/notifications/internal/tasks.ts
@@ -1,6 +1,11 @@
 import { Task } from '@awarns/core/tasks';
 import { NotificationSenderTask } from './sender';
+import { RandomNotificationSenderTask } from './random-sender';
 
 export function sendNotificationTask(): Task {
   return new NotificationSenderTask('sendNotification');
+}
+
+export function sendRandomNotificationTask(): Task {
+  return new RandomNotificationSenderTask('sendRandomNotification');
 }
