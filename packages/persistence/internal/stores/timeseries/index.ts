e1f16d452999c4211a259c10e0cf48dfddda343b
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-18 19:22:20 +0100

    feat(persistence): expose missing local time series store interface

--- packages/persistence/internal/stores/timeseries/index.ts
+++ packages/persistence/internal/stores/timeseries/index.ts
@@ -1,7 +1,13 @@
 import { TimeSeriesSyncedStore } from './synchronizer';
-import { TimeSeriesEntity as TSE, TimeSeriesStore as TSS, TimeSeriesDoc as TSD } from './common';
+import {
+  TimeSeriesEntity as TSE,
+  TimeSeriesStore as TSS,
+  LocalTimeSeriesStore as LTSS,
+  TimeSeriesDoc as TSD,
+} from './common';
 export type TimeSeriesEntity = TSE;
 export type TimeSeriesStore<T extends TimeSeriesEntity> = TSS<T>;
+export type LocalTimeSeriesStore<T extends TimeSeriesEntity> = LTSS<T>;
 export type TimeSeriesDoc = TSD;
 
 export { TimeSeriesSyncedStore };
