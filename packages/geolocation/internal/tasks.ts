eb8225d37be870efbb5026d6388afb13a2e9c78b
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-27 15:08:47 +0100

    fix(geoloc): tasks' setup will fail when config object is missing

--- packages/geolocation/internal/tasks.ts
+++ packages/geolocation/internal/tasks.ts
@@ -7,23 +7,19 @@ const DEFAULT_SINGLE_TIMEOUT = 10000;
 const DEFAULT_BATCH_BEST_OF = 1;
 const DEFAULT_BATCH_TIMEOUT = 15000;
 
-export function acquirePhoneGeolocationTask(config?: GeolocationTaskConfig): Task {
+export function acquirePhoneGeolocationTask(config: GeolocationTaskConfig = {}): Task {
   return new SinglePullProviderTask(
     new GeolocationProvider(config.bestOf ?? DEFAULT_SINGLE_BEST_OF, config.timeout ?? DEFAULT_SINGLE_TIMEOUT),
     'Phone',
-    {
-      foreground: true,
-    }
+    { foreground: true }
   );
 }
 
-export function acquireMultiplePhoneGeolocationTask(config?: GeolocationTaskConfig): Task {
+export function acquireMultiplePhoneGeolocationTask(config: GeolocationTaskConfig = {}): Task {
   return new BatchPullProviderTask(
     new GeolocationProvider(config.bestOf ?? DEFAULT_BATCH_BEST_OF, config.timeout ?? DEFAULT_BATCH_TIMEOUT),
     'Phone',
-    {
-      foreground: true,
-    }
+    { foreground: true }
   );
 }
 
