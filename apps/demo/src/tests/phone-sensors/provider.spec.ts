623e6ad4b1968389e998f84267ab79d643c4eff6
Miguel Matey Sanz <al341802@uji.es>
2022-08-01 15:59:28 +0200

    test(phone-sensors): add provider and receiver tests

---
+++ apps/demo/src/tests/phone-sensors/provider.spec.ts
@@ -0,0 +1,76 @@
+import { PhoneSensorsProvider, ProviderConfiguration } from '@awarns/phone-sensors/internal/provider';
+import { PhoneSensor, toNativeSensor } from '@awarns/phone-sensors/internal/phone-sensor';
+
+describe('Phone sensors provider', () => {
+  const sensor = PhoneSensor.ACCELEROMETER;
+  const nativeSensor = toNativeSensor(sensor);
+  const config: ProviderConfiguration = {
+    sensorDelay: 10,
+    batchSize: 50,
+  };
+
+  let sensorManager;
+  let serviceManager;
+
+  let provider: PhoneSensorsProvider;
+
+  beforeEach(() => {
+    sensorManager = createSensorManagerMock();
+    serviceManager = createServiceManagerMock();
+
+    provider = new PhoneSensorsProvider(sensor, config, sensorManager, serviceManager);
+  });
+
+  it('allows to check if the sensor is present in the device', async () => {
+    spyOn(sensorManager, 'isSensorAvailable').and.returnValue(true);
+    await provider.checkIfIsReady();
+    expect(sensorManager.isSensorAvailable).toHaveBeenCalled();
+  });
+
+  it('throws an error when the sensor is not present in the device', async () => {
+    spyOn(sensorManager, 'isSensorAvailable').and.returnValue(false);
+    await expectAsync(provider.checkIfIsReady()).toBeRejectedWith(
+      new Error(`Sensor ${sensor} is not ready! This sensor might not be available in this device!`)
+    );
+  });
+
+  it('instructs the service manager to start the data collection', async () => {
+    spyOn(serviceManager, 'startCollection').and.callThrough();
+    const nativeConfig = collectionConfigurationMock(nativeSensor, config);
+    await provider.startProviding();
+    expect(serviceManager.startCollection).toHaveBeenCalledWith(nativeConfig, PhoneSensorsProvider.callback);
+  });
+
+  it('instructs the service manager to stop the data collection', async () => {
+    spyOn(serviceManager, 'stopCollection').and.callThrough();
+    await provider.stopProviding();
+    expect(serviceManager.stopCollection).toHaveBeenCalledWith(nativeSensor);
+  });
+});
+
+function createSensorManagerMock() {
+  return {
+    isSensorAvailable(sensor) {
+      return false;
+    },
+    availableSensors() {
+      return undefined;
+    },
+  };
+}
+
+function createServiceManagerMock() {
+  return {
+    startCollection(config, callback) {},
+    stopCollection(sensor) {},
+  };
+}
+
+function collectionConfigurationMock(sensor, config: ProviderConfiguration) {
+  // @ts-ignore
+  return new es.uji.geotec.backgroundsensors.collection.CollectionConfiguration(
+    sensor,
+    config.sensorDelay * 1000,
+    config.batchSize
+  );
+}
---
+++ apps/demo/src/tests/phone-sensors/receiver.spec.ts
@@ -0,0 +1,61 @@
+import { TriAxialReceiver } from '@awarns/phone-sensors/internal/receiver';
+import { EventData } from '@awarns/core/events';
+import { TriAxial } from '@awarns/phone-sensors/internal/tri-axial';
+import { PhoneSensor } from '@awarns/phone-sensors/internal/phone-sensor';
+
+const now = new Date().getTime();
+
+describe('TriAxial receiver', () => {
+  let eventEmitter: (eventName: string, eventData?: EventData) => void;
+  let receiver: TriAxialReceiver;
+
+  beforeEach(() => {
+    eventEmitter = jasmine.createSpy();
+    receiver = new TriAxialReceiver(eventEmitter);
+  });
+
+  it('does nothing when receives an empty list of native records', () => {
+    receiver.onReceive(createFakeNativeRecords(0));
+    expect(eventEmitter).not.toHaveBeenCalled();
+  });
+
+  it('emits an event with the received records', () => {
+    const expectedRecord = new TriAxial(
+      PhoneSensor.ACCELEROMETER,
+      [
+        { x: 0, y: 0, z: 0, detectedAt: new Date(now) },
+        { x: 1, y: 1, z: 1, detectedAt: new Date(now + 1000) },
+        { x: 2, y: 2, z: 2, detectedAt: new Date(now + 2000) },
+      ],
+      new Date(now)
+    );
+    const { id, ...expectedProperties } = expectedRecord;
+    receiver.onReceive(createFakeNativeRecords(3));
+
+    expect(eventEmitter).toHaveBeenCalledWith(
+      'accelerometerSamplesAcquired',
+      jasmine.objectContaining(expectedProperties)
+    );
+  });
+});
+
+function createFakeNativeRecords(size: number) {
+  // @ts-ignore
+  const arr = new java.util.LinkedList<es.uji.geotec.backgroundsensors.record.TriAxialRecord>();
+
+  for (let i = 0; i < size; i++) {
+    arr.add(
+      // @ts-ignore
+      new es.uji.geotec.backgroundsensors.record.TriAxialRecord(
+        // @ts-ignore
+        es.uji.geotec.backgroundsensors.sensor.BaseSensor.ACCELEROMETER,
+        now + i * 1000,
+        i,
+        i,
+        i
+      )
+    );
+  }
+
+  return arr;
+}
