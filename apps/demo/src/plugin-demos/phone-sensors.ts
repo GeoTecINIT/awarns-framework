38c95d234064c3945aed084cea72daef4dd861d0
Miguel Matey Sanz <al341802@uji.es>
2022-08-02 17:25:43 +0200

    fix(phone-sensors): lint issues

--- apps/demo/src/plugin-demos/phone-sensors.ts
+++ apps/demo/src/plugin-demos/phone-sensors.ts
@@ -1,6 +1,5 @@
-import { Observable, EventData, Page } from '@nativescript/core';
+import { EventData, Page } from '@nativescript/core';
 import { DemoSharedPhoneSensors } from '@demo/shared';
-import {} from '@awarns/phone-sensors';
 
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object;
