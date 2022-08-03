06d2d0fcfe14d28da53cab0c329a9ee5811cacc7
Miguel Matey Sanz <al341802@uji.es>
2022-08-03 17:37:47 +0200

    feat(wear-os): way to enable plugin features and indicate watch to use

---
+++ packages/wear-os/internal/setup/index.ts
@@ -0,0 +1,31 @@
+import { Watch } from '../watch';
+import { setBoolean, getBoolean, setString, getString, flush } from '@nativescript/core/application-settings';
+import { Node } from 'nativescript-wearos-sensors/node';
+
+const WATCH_FEATURES_ENABLED = 'WATCH_FEATURES_ENABLED_KEY';
+const CURRENT_WATCH = 'CURRENT_WATCH_KEY';
+
+export function setWatchFeaturesState(enabled: boolean): void {
+  setBoolean(WATCH_FEATURES_ENABLED, enabled);
+  flush();
+}
+
+export function areWatchFeaturesEnabled(): boolean {
+  return getBoolean(WATCH_FEATURES_ENABLED, true);
+}
+
+export function useWatch(watch: Watch): void {
+  const stringifiedWatch = `${watch.id}#${watch.name}#${JSON.stringify(watch.capabilities)}`;
+  setString(CURRENT_WATCH, stringifiedWatch);
+  flush();
+}
+
+export function getWatchInUse(): Watch {
+  const stringifiedWatch = getString(CURRENT_WATCH);
+  if (!stringifiedWatch) {
+    throw new Error('There is no watch selected for use!');
+  }
+
+  const watchProperties = stringifiedWatch.split('#');
+  return new Node(watchProperties[0], watchProperties[1], JSON.parse(watchProperties[2]));
+}
