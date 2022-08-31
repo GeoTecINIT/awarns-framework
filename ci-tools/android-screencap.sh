30da4fbc990840611fd14d403bb2a4a98b1b2264
Alberto González <10727467+agonper@users.noreply.github.com>
2022-08-31 12:00:31 +0200

    ci: capture screenshots iteratively

--- ci-tools/android-screencap.sh
+++ ci-tools/android-screencap.sh
@@ -1,1 +1,15 @@
-adb devices -l | tail -n +2 | cut -d' ' -f1 | xargs -n1 -I{} sh -c "adb -s {} exec-out screencap -p > screenshots/{}_$(date +'%Y-%m-%d_%H-%M-%S').png"
+screenshots_dir="screenshots"
+screenshots_count=300
+wait_time=1
+
+mkdir $screenshots_dir
+
+count=1
+while [ $count -le $screenshots_count ]; do
+  echo "Capturing screenshot #$count"
+  adb devices -l | tail -n +2 | cut -d' ' -f1 | xargs -n1 -I{} sh -c "adb -s {} exec-out screencap -p > $screenshots_dir/{}_$(date +'%Y-%m-%d_%H-%M-%S').png"
+  sleep $wait_time
+  ((count++))
+done
+
+ls -l $screenshots_dir
