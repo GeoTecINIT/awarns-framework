672ee27127de1f87d0eb3a1930e60709fe66e8cc
Alberto González <10727467+agonper@users.noreply.github.com>
2022-08-31 16:21:28 +0200

    ci: record more than one video

--- ci-tools/android-screenrec.sh
+++ ci-tools/android-screenrec.sh
@@ -1,12 +1,25 @@
 screenshots_dir="screenshots"
+time=$1
+reps=$2
+
+function record() {
+    num=$1
+
+    echo "Recording device screen for $time seconds ($num/$reps)"
+
+    video_path="/sdcard/video-$num.mp4"
+    adb shell screenrecord $video_path --time-limit $time
+    adb pull $video_path
+    adb shell rm $video_path
+}
+
 mkdir $screenshots_dir
 cd $screenshots_dir
 
-echo "Recording device screen for $1 seconds"
-
-video_path="/sdcard/video.mp4"
-adb shell screenrecord $video_path --time-limit $1
-adb pull $video_path
-adb shell rm $video_path
+count=1
+while [ $count -le $reps ]; do
+    record $count
+    ((count++))
+done
 
 cd ..
