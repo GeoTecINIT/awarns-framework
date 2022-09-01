81b706f9d6539122e23bde8b58427b354fd9b3e3
Alberto González <10727467+agonper@users.noreply.github.com>
2022-09-01 13:09:15 +0200

    ci: make log script resilient to app restarts

--- ci-tools/android-log.sh
+++ ci-tools/android-log.sh
@@ -1,9 +1,21 @@
-pid=""
+pid=-1
+log_pid=-1
 
 echo "Waiting for app to start..."
-while [ -z $pid ]; do
-  pid=$(adb shell pidof org.nativescript.plugindemo)
+while [ 1 ]; do
+  new_pid=$(adb shell pidof org.nativescript.plugindemo)
+  if ! [ -z $new_pid ] && [ $new_pid -ne $pid ]; then
+
+    if [ $log_pid -ne -1 ]; then
+      echo "Killing previous log process ($log_pid)"
+      kill $log_pid
+    fi
+
+    pid=$new_pid
+
+    echo "App start with PID: $pid. Printing logs..."
+    adb logcat --pid=$pid &
+    log_pid=$!
+  fi
   sleep 1
 done
-echo "App started with PID: $pid. Printing logs..."
-adb logcat --pid=$pid
