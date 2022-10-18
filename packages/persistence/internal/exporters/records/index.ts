3dda71f7b8e5881713f1c0fe533d33006c749cf5
Miguel Matey Sanz <al341802@uji.es>
2022-10-18 09:03:37 +0200

    refactor(persistence): replace optional parameters by config object in exporter

--- packages/persistence/internal/exporters/records/index.ts
+++ packages/persistence/internal/exporters/records/index.ts
@@ -3,17 +3,21 @@ import { Exporter, ExportFormats } from '../index';
 import { CSVRecordsExporter } from './csv';
 import { JSONRecordsExporter } from './json';
 
+export interface RecordsExporterOptions {
+  fileName?: string;
+  recordTypes?: string[];
+}
+
 export function createRecordsExporter(
   folder: Folder,
   format: ExportFormats = 'csv',
-  fileName?: string,
-  recordType?: string
+  options?: RecordsExporterOptions
 ): Exporter {
   switch (format) {
     case 'csv':
-      return new CSVRecordsExporter(folder, fileName, recordType);
+      return new CSVRecordsExporter(folder, options.fileName, options.recordTypes);
     case 'json':
-      return new JSONRecordsExporter(folder, fileName, recordType);
+      return new JSONRecordsExporter(folder, options.fileName, options.recordTypes);
   }
 }
 
